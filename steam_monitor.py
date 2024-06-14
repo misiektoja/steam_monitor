538e15de1c739b62b5a0e2411f1a106a1355ddda
misiektoja <misiektoja-github@rm-rf.ninja>
2024-06-14 22:07:47 +0200

    Fixed f-string syntax errors in older Python versions + support for float type of timestamps added in date/time related functions + get_short_date_from_ts() rewritten to display year if show_year == True and current year is different, also can omit displaying hour and minutes if show_hours == False

--- steam_monitor.py
+++ steam_monitor.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
 Author: Michal Szymanski <misiektoja-github@rm-rf.ninja>
-v1.1
+v1.2
 
-Script implementing real-time monitoring of Steam players activity:
+Tool implementing real-time tracking of Steam players activities:
 https://github.com/misiektoja/steam_monitor/
 
 Python pip3 requirements:
@@ -13,7 +13,7 @@ python-dateutil
 requests
 """
 
-VERSION = 1.1
+VERSION = 1.2
 
 # ---------------------------
 # CONFIGURATION SECTION START
@@ -30,12 +30,12 @@ SMTP_USER = "your_smtp_user"
 SMTP_PASSWORD = "your_smtp_password"
 SMTP_SSL = True
 SENDER_EMAIL = "your_sender_email"
-#SMTP_HOST = "mail.upcpoczta.pl"
-#SMTP_PORT = 25
-#SMTP_USER = "asz180312@upcpoczta.pl"
-#SMTP_PASSWORD = "vWyuW7wrZGy7W45"
-#SMTP_SSL = False
-#SENDER_EMAIL = "asz180312@upcpoczta.pl"
+# SMTP_HOST = "your_smtp_server_plaintext"
+# SMTP_PORT = 25
+# SMTP_USER = "your_smtp_user"
+# SMTP_PASSWORD = "your_smtp_password"
+# SMTP_SSL = False
+# SENDER_EMAIL = "your_sender_email"
 RECEIVER_EMAIL = "your_receiver_email"
 
 # How often do we perform checks for player activity when user is offline, you can also use -c parameter; in seconds
@@ -77,6 +77,9 @@ steam_visibilitystates = ["private", "private", "private", "public"]
 active_inactive_notification = False
 game_change_notification = False
 status_notification = False
+
+# to solve the issue: 'SyntaxError: f-string expression part cannot include a backslash'
+nl_ch = "\n"
 
 
 import sys
@@ -175,6 +178,9 @@ def calculate_timespan(timestamp1, timestamp2, show_weeks=True, show_hours=True,
 
     if type(timestamp1) is int:
         dt1 = datetime.fromtimestamp(int(ts1))
+    elif type(timestamp1) is float:
+        ts1 = int(round(ts1))
+        dt1 = datetime.fromtimestamp(ts1)
     elif type(timestamp1) is datetime:
         dt1 = timestamp1
         ts1 = int(round(dt1.timestamp()))
@@ -183,6 +189,9 @@ def calculate_timespan(timestamp1, timestamp2, show_weeks=True, show_hours=True,
 
     if type(timestamp2) is int:
         dt2 = datetime.fromtimestamp(int(ts2))
+    elif type(timestamp2) is float:
+        ts2 = int(round(ts2))
+        dt2 = datetime.fromtimestamp(ts2)
     elif type(timestamp2) is datetime:
         dt2 = timestamp2
         ts2 = int(round(dt2.timestamp()))
@@ -307,7 +316,7 @@ def write_csv_entry(csv_file_name, timestamp, status, gamename, gameid):
 
 # Function to return the timestamp in human readable format; eg. Sun, 21 Apr 2024, 15:08:45
 def get_cur_ts(ts_str=""):
-    return (f"{ts_str}{calendar.day_abbr[(datetime.fromtimestamp(int(time.time()))).weekday()]}, {datetime.fromtimestamp(int(time.time())).strftime("%d %b %Y, %H:%M:%S")}")
+    return (f'{ts_str}{calendar.day_abbr[(datetime.fromtimestamp(int(time.time()))).weekday()]}, {datetime.fromtimestamp(int(time.time())).strftime("%d %b %Y, %H:%M:%S")}')
 
 
 # Function to print the current timestamp in human readable format; eg. Sun, 21 Apr 2024, 15:08:45
@@ -322,22 +331,41 @@ def get_date_from_ts(ts):
         ts_new = int(round(ts.timestamp()))
     elif type(ts) is int:
         ts_new = ts
+    elif type(ts) is float:
+        ts_new = int(round(ts))
     else:
         return ""
 
-    return (f"{calendar.day_abbr[(datetime.fromtimestamp(ts_new)).weekday()]} {datetime.fromtimestamp(ts_new).strftime("%d %b %Y, %H:%M:%S")}")
+    return (f'{calendar.day_abbr[(datetime.fromtimestamp(ts_new)).weekday()]} {datetime.fromtimestamp(ts_new).strftime("%d %b %Y, %H:%M:%S")}')
 
 
-# Function to return the timestamp/datetime object in human readable format (short version); eg. Sun 21 Apr 15:08
-def get_short_date_from_ts(ts):
+# Function to return the timestamp/datetime object in human readable format (short version); eg.
+# Sun 21 Apr 15:08
+# Sun 21 Apr 24, 15:08 (if show_year == True and current year is different)
+# Sun 21 Apr (if show_hour == False)
+def get_short_date_from_ts(ts, show_year=False, show_hour=True):
     if type(ts) is datetime:
         ts_new = int(round(ts.timestamp()))
     elif type(ts) is int:
         ts_new = ts
+    elif type(ts) is float:
+        ts_new = int(round(ts))
     else:
         return ""
 
-    return (f"{calendar.day_abbr[(datetime.fromtimestamp(ts_new)).weekday()]} {datetime.fromtimestamp(ts_new).strftime("%d %b %H:%M")}")
+    if show_hour:
+        hour_strftime = " %H:%M"
+    else:
+        hour_strftime = ""
+
+    if show_year and int(datetime.fromtimestamp(ts_new).strftime("%Y")) != int(datetime.now().strftime("%Y")):
+        if show_hour:
+            hour_prefix = ","
+        else:
+            hour_prefix = ""
+        return (f'{calendar.day_abbr[(datetime.fromtimestamp(ts_new)).weekday()]} {datetime.fromtimestamp(ts_new).strftime(f"%d %b %y{hour_prefix}{hour_strftime}")}')
+    else:
+        return (f'{calendar.day_abbr[(datetime.fromtimestamp(ts_new)).weekday()]} {datetime.fromtimestamp(ts_new).strftime(f"%d %b{hour_strftime}")}')
 
 
 # Function to return the timestamp/datetime object in human readable format (only hour, minutes and optionally seconds): eg. 15:08:12
@@ -346,6 +374,8 @@ def get_hour_min_from_ts(ts, show_seconds=False):
         ts_new = int(round(ts.timestamp()))
     elif type(ts) is int:
         ts_new = ts
+    elif type(ts) is float:
+        ts_new = int(round(ts))
     else:
         return ""
 
@@ -362,6 +392,8 @@ def get_range_of_dates_from_tss(ts1, ts2, between_sep=" - ", short=False):
         ts1_new = int(round(ts1.timestamp()))
     elif type(ts1) is int:
         ts1_new = ts1
+    elif type(ts1) is float:
+        ts1_new = int(round(ts1))
     else:
         return ""
 
@@ -369,6 +401,8 @@ def get_range_of_dates_from_tss(ts1, ts2, between_sep=" - ", short=False):
         ts2_new = int(round(ts2.timestamp()))
     elif type(ts2) is int:
         ts2_new = ts2
+    elif type(ts2) is float:
+        ts2_new = int(round(ts2))
     else:
         return ""
 
@@ -586,7 +620,7 @@ def steam_monitor_user(steamid, error_notification, csv_file_name, csv_exists):
     if "games" in s_played["response"].keys():
         print(f"\nList of recently played games:")
         for i, game in enumerate(s_played["response"]["games"]):
-            print(f"{i + 1} {game.get("name")}")
+            print(f"{i + 1} {game.get('name')}")
 
     status_old = status
     gameid_old = gameid
@@ -619,7 +653,7 @@ def steam_monitor_user(steamid, error_notification, csv_file_name, csv_exists):
                 print("* API key might not be valid anymore!")
                 if error_notification and not email_sent:
                     m_subject = f"steam_monitor: API key error! (user: {username})"
-                    m_body = f"API key might not be valid anymore: {e}{get_cur_ts("\n\nTimestamp: ")}"
+                    m_body = f"API key might not be valid anymore: {e}{get_cur_ts(nl_ch + nl_ch + 'Timestamp: ')}"
                     print(f"Sending email notification to {RECEIVER_EMAIL}")
                     send_email(m_subject, m_body, "", SMTP_SSL)
                     email_sent = True
@@ -701,7 +735,7 @@ def steam_monitor_user(steamid, error_notification, csv_file_name, csv_exists):
             change = True
 
             m_subject = f"Steam user {username} is now {steam_personastates[status]} (after {m_subject_after}{m_subject_was_since})"
-            m_body = f"Steam user {username} changed status from {steam_personastates[status_old]} to {steam_personastates[status]}\n\nUser was {steam_personastates[status_old]} for {calculate_timespan(int(status_ts), int(status_ts_old))}{m_body_was_since}{m_body_short_offline_msg}{m_body_user_in_game}{m_body_played_games}{get_cur_ts("\n\nTimestamp: ")}"
+            m_body = f"Steam user {username} changed status from {steam_personastates[status_old]} to {steam_personastates[status]}\n\nUser was {steam_personastates[status_old]} for {calculate_timespan(int(status_ts), int(status_ts_old))}{m_body_was_since}{m_body_short_offline_msg}{m_body_user_in_game}{m_body_played_games}{get_cur_ts(nl_ch + nl_ch + 'Timestamp: ')}"
             if status_notification or (active_inactive_notification and act_inact_flag):
                 print(f"Sending email notification to {RECEIVER_EMAIL}")
                 send_email(m_subject, m_body, "", SMTP_SSL)
@@ -714,27 +748,27 @@ def steam_monitor_user(steamid, error_notification, csv_file_name, csv_exists):
             # User changed the game
             if gameid_old and gameid:
                 print(f"Steam user {username} changed game from '{gamename_old}' to '{gamename}' after {calculate_timespan(int(game_ts), int(game_ts_old))}")
-                print(f"User played game from {get_range_of_dates_from_tss(int(game_ts_old), int(game_ts), short=True, between_sep=" to ")}")
+                print(f"User played game from {get_range_of_dates_from_tss(int(game_ts_old), int(game_ts), short=True, between_sep=' to ')}")
                 game_total_ts += (int(game_ts) - int(game_ts_old))
                 games_number += 1
                 m_subject = f"Steam user {username} changed game to '{gamename}' (after {calculate_timespan(int(game_ts), int(game_ts_old), show_seconds=False)}: {get_range_of_dates_from_tss(int(game_ts_old), int(game_ts), short=True)})"
-                m_body = f"Steam user {username} changed game from '{gamename_old}' to '{gamename}' after {calculate_timespan(int(game_ts), int(game_ts_old))}\n\nUser played game from {get_range_of_dates_from_tss(int(game_ts_old), int(game_ts), short=True, between_sep=" to ")}{get_cur_ts("\n\nTimestamp: ")}"
+                m_body = f"Steam user {username} changed game from '{gamename_old}' to '{gamename}' after {calculate_timespan(int(game_ts), int(game_ts_old))}\n\nUser played game from {get_range_of_dates_from_tss(int(game_ts_old), int(game_ts), short=True, between_sep=' to ')}{get_cur_ts(nl_ch + nl_ch + 'Timestamp: ')}"
 
             # User started playing new game
             elif not gameid_old and gameid:
                 print(f"Steam user {username} started playing '{gamename}'")
                 games_number += 1
                 m_subject = f"Steam user {username} now plays '{gamename}'"
-                m_body = f"Steam user {username} now plays '{gamename}'{get_cur_ts("\n\nTimestamp: ")}"
+                m_body = f"Steam user {username} now plays '{gamename}'{get_cur_ts(nl_ch + nl_ch + 'Timestamp: ')}"
 
             # User stopped playing the game
             elif gameid_old and not gameid:
                 print(f"Steam user {username} stopped playing '{gamename_old}' after {calculate_timespan(int(game_ts), int(game_ts_old))}")
-                print(f"User played game from {get_range_of_dates_from_tss(int(game_ts_old), int(game_ts), short=True, between_sep=" to ")}")
+                print(f"User played game from {get_range_of_dates_from_tss(int(game_ts_old), int(game_ts), short=True, between_sep=' to ')}")
                 if not game_total_after_offline_counted:
                     game_total_ts += (int(game_ts) - int(game_ts_old))
                 m_subject = f"Steam user {username} stopped playing '{gamename_old}' (after {calculate_timespan(int(game_ts), int(game_ts_old), show_seconds=False)}: {get_range_of_dates_from_tss(int(game_ts_old), int(game_ts), short=True)})"
-                m_body = f"Steam user {username} stopped playing '{gamename_old}' after {calculate_timespan(int(game_ts), int(game_ts_old))}\n\nUser played game from {get_range_of_dates_from_tss(int(game_ts_old), int(game_ts), short=True, between_sep=" to ")}{get_cur_ts("\n\nTimestamp: ")}"
+                m_body = f"Steam user {username} stopped playing '{gamename_old}' after {calculate_timespan(int(game_ts), int(game_ts_old))}\n\nUser played game from {get_range_of_dates_from_tss(int(game_ts_old), int(game_ts), short=True, between_sep=' to ')}{get_cur_ts(nl_ch + nl_ch + 'Timestamp: ')}"
 
             change = True
 
