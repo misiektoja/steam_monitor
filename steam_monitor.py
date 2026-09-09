ccd80ae7c9cca8e36e762580475815d86542933c
misiektoja <misiektoja-github@rm-rf.ninja>
2026-09-09 11:25:41 +0200

    fix(diagnostics): print the liveness banner once a day by default

--- steam_monitor.py
+++ steam_monitor.py
@@ -243,7 +243,7 @@ STEAM_SNOOZE_INACTIVITY_THRESHOLD = 7200  # 2 hours
 
 # How often to print a "liveness check" message to the output; in seconds
 # Set to 0 to disable
-LIVENESS_CHECK_INTERVAL = 43200  # 12 hours
+LIVENESS_CHECK_INTERVAL = 86400  # 24 hours
 
 # URL used to verify internet connectivity at startup
 CHECK_INTERNET_URL = 'https://api.steampowered.com/'
